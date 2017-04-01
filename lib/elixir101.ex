23ede851ae1747c82a1017f3e8e822889d44fdb8
Zhang Yi <zhangyi@bigeyedata.com>
2017-04-01 22:31:48 +0800

    add mix project

---
+++ lib/elixir101.ex
@@ -0,0 +1,10 @@
+defmodule Elixir101 do
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    Appl
+  end
+  
+end
---
+++ test/elixir101_test.exs
@@ -0,0 +1,9 @@
+defmodule Elixir101Test do
+  use ExUnit.Case
+  doctest Elixir101
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+
+end
