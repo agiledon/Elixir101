7dbc45e5f16f5e54e0a49a17fb57c73f7d925db0
Zhang Yi <zhangyi@bigeyedata.com>
2017-04-04 21:52:31 +0800

    add the test case to cover scoping rule for with expression

--- test/basic/with_test.exs
+++ test/basic/with_test.exs
@@ -33,4 +33,15 @@ defmodule Basic.WithTest do
   test "return error giving wrong opts" do
     assert area_for_wrong_case() == :error
   end
+
+  test "scoping rule used in with expression" do
+    width = nil
+    opts = %{width: 10, height: 15}
+    assert {:ok, 300} ==
+        with {:ok, width} <- Map.fetch(opts, :width),
+             double_width  = width * 2,
+             {:ok, height} <- Map.fetch(opts, :height),
+        do: {:ok, double_width * height}
+    assert width == nil
+  end
 end
